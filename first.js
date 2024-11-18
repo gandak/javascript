4d328a301820fedaafd69b9633588a53df08518d
24LP3548 24LP3548 <24LP3548@G304-CC0220.local>
2024-11-18 12:38:28 +0800

    java first code

---
+++ first.js
@@ -0,0 +1,40 @@
+// let birthYear = 1991;
+// let yearNow = 2024;
+// console.log(yearNow - birthYear);
+
+let title = "BABY SHARK";
+let baby = "Baby shark, ";
+let daddy = "Daddy shark, ";
+let mommy = "Mommy shark, ";
+let line = "doo, doo, doo, doo, doo, doo";
+let grandma = "Grandma shark, ";
+let grandpa = "Grandpa shark, ";
+let hunt = "Let's go hunt, ";
+let run = "Run away, ";
+let safe = "Safe at last, ";
+let end = "It's the end, ";
+console.log(
+  baby + line,
+  "\n",
+  baby + line,
+  "\n",
+  baby + line,
+  "\n",
+  "Baby shark"
+);
+console.log(
+  mommy + line,
+  "\n",
+  mommy + line,
+  "\n",
+  mommy + line,
+  "\n",
+  "Mommy shark"
+);
+console.log(daddy, line, "\n", line, "\n", "Daddy shark");
+console.log(grandma, line, "\n", line, "\n", "Grandma shark");
+console.log(grandpa, line, "\n", line, "\n", "Grandpa shark");
+console.log(hunt, line, "\n", line, "\n", "Let's go hunt");
+console.log(run, line, "\n", line, "\n", "Run away");
+console.log(safe, line, "\n", line, "\n", "Safe at last");
+console.log(end, line, "\n", line, "\n", "It's the end");
---
+++ jolootest.js
@@ -0,0 +1,16 @@
+let drom = true;
+let onoo = 20;
+
+if (drom == true) {
+  if (onoo > 18) {
+    console.log("tentssen");
+  } else {
+    console.log("duremdee tentseh yostoi");
+  }
+} else {
+  if (onoo < 18) {
+    console.log("2uuland ni tentseh yostoi");
+  } else {
+    console.log("dromdoo tentseh yostoi");
+  }
+}
